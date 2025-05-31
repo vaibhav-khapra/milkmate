9bedef5b5a63af16be86b74317a1eb91bea8fa0b
vaibhav-khapra <vaibhavkhapra5@gmail.com>
2025-05-31 08:48:13 +0530

    Delete customer Updated

--- app/api/deletecustomer/route.js
+++ app/api/deletecustomer/route.js
@@ -1,16 +1,30 @@
 import { NextResponse } from 'next/server'
 import connectToDatabase from '@/app/conn/db'
 import Customer from '@/models/Customer'
+import Bill from '@/models/Bill'
+import Extra from '@/models/Extra'
+import Undelivered from '@/models/Undelivered'
 
 export async function DELETE(req) {
     try {
         await connectToDatabase()
         const body = await req.json()
         const { id } = body
+        const customer = await Customer.findById(id);
+        if (!customer) {
+            return NextResponse.json({ error: 'Customer not found' }, { status: 404 });
+          }
+        const customerName = customer.name;
 
+        const deletedbill = await Bill.deleteMany({ customerName });
+        const deletedextra = await Extra.deleteMany({ name: customer.name });
+        const deletedundel = await Undelivered.deleteMany({ name: customer.name });
+
+       
+       
         const deleted = await Customer.findByIdAndDelete(id)
 
-        if (!deleted) {
+        if (!deleted && !deletedbill && !deletedextra && !deletedundel) {
             return NextResponse.json({ success: false, message: 'Customer not found' })
         }
 
